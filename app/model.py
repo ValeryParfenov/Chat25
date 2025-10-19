01232214c4ae9be02aa98df609e8ce596dc387d1
Valery Parfenov <parfenov.ve@phystech.edu>
2025-10-19 16:35:05 +0300

    Model was defined

---
+++ app/model.py
@@ -0,0 +1,22 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+MODEL_NAME = "gpt2"
+
+_tokenizer = None
+_model = None
+
+def _load_model():
+    global _tokenizer, _model
+    if _tokenizer is None or _model is None:
+        _tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+        _model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
+    return _tokenizer, _model
+
+
+def generate_response(prompt: str, max_new_tokens: int = 40) -> str:
+    tokenizer, model = _load_model()
+    inputs = tokenizer(prompt, return_tensors="pt")
+    outputs = model.generate(**inputs, max_new_tokens=max_new_tokens)
+    text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return text
