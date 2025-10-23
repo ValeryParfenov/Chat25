ba4dad22ebe743ff9a15ba61d4565dc034631e90
Pavel Lozhkin <lozhkin.pavel@gmail.com>
2025-10-24 00:16:11 +0300

    add benchmark tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,6 @@
 import unittest
 from app.model import generate_response
+from time import time
 
 class TestModel(unittest.TestCase):
     def test_returns_string(self):
@@ -12,3 +13,30 @@ class TestModel(unittest.TestCase):
         response = generate_response(prompt)
         self.assertIsInstance(response, str)
         self.assertIn("The", response[:100])
+
+
+class Benchmark(unittest.TestCase):
+    def test_time(self):
+        prompt = "Hello, how are you?"
+        n = 10
+        avg_duration = 0.0
+        for _ in range(n):
+            start = time()
+            generate_response(prompt)
+            end = time()
+            avg_duration += end - start
+        avg_duration /= n
+        self.assertTrue(avg_duration < 1.0)
+        
+    def test_time_per_token(self):
+        prompt = "Hello, how are you?"
+        n = 10
+        avg_duration_per_word = 0.0
+        for _ in range(n):
+            start = time()
+            response = generate_response(prompt)
+            end = time()
+            avg_duration_per_word += (end - start) / len(response.split(' '))
+        avg_duration_per_word /= n
+        print(avg_duration_per_word)
+        self.assertTrue(avg_duration_per_word < 0.05)
