6ee61c75f60492ff0c8e309ab8ad048a8c6fc3fc
awesomeslayer <fatalick01@gmail.com>
2025-10-24 15:35:22 +0300

    launch script

--- scripts/launch_container.sh
+++ scripts/launch_container.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 source $( dirname ${BASH_SOURCE[0]} )/credentials.sh
 
 docker run \
@@ -8,5 +9,8 @@ docker run \
   --user ${DOCKER_USER_ID}:${DOCKER_GROUP_ID} \
   --name ${CONTAINER_NAME} \
   --rm -it --init \
+  --gpus '"device=5"' \
+  -v /home/e.serov/Chat25:/app \
   -p 7860:7860 \
   ${DOCKER_NAME} 
+
